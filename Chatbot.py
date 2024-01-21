1b0f3ac5d431b6217d31f116c655d59ea9f3434f
ManRev <mrevel@mit.edu>
2024-01-21 17:51:13 -0500

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -2,13 +2,7 @@ from openai import OpenAI
 import streamlit as st
 
 # Retrieve OpenAI API key from st.secrets
-openai_api_key = st.secrets.get("OPENAI_API_KEY", "")
-
-with st.sidebar:
-    #st.text_input("OpenAI API Key", key="chatbot_api_key", type="password", value=openai_api_key)
-    "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
-    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
-    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+openai_api_key = st.secrets["OPENAI_API_KEY"]
 
 st.title("💬 Chatbot")
 st.caption("🚀 A streamlit chatbot powered by OpenAI LLM")
