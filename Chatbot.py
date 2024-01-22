bb88a95a76acb1e1de1abb64c290a0bd1b620005
ManRev <mrevel@mit.edu>
2024-01-22 12:34:37 -0500

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -18,7 +18,7 @@ if prompt := st.chat_input():
         st.stop()
 
     client = OpenAI(api_key=openai_api_key)
-    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.session_state.messages.append({"role": "user", "content": "Please, answer the following prompt always starting your sentences with the letter A. " + prompt})
     st.chat_message("user").write(prompt)
     response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
     msg = response.choices[0].message.content
